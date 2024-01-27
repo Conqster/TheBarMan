5d7d2c7f4413f206185a6d71fce7f0afd8faeaac
Zodiac411 <thechriszone@icloud.com>
2024-01-27 09:52:57 +0000

    Added player and base drinks

---
+++ Assets/Scripts/ScriptableObjects/Drinks.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "Create Drink", menuName ="Drink")]
+public class Drinks : ScriptableObject
+{
+    public GameObject DrinkObject;
+   public string drinkName;
+    
+}
